8ab807daed30316e4a41bb37c77984cfadfe4f14
Brent Westbrook <36778786+ntBre@users.noreply.github.com>
2024-11-19 12:39:31 -0500

    Add Parsley 1.3.1 (#12)
    * Add Parsley 1.3.1
    
    * Add benchmark results
    
    * tar the results and then upload as a github archive
    
    * more error handling in check_status
    
    * add retries, remove fatal argument to check_status
    
    * move zenodo url to env for testing
    
    * Add benchmark results
    
    * bzip db and export env before trying to tar them
    
    * skip files that don't exist for easier tar usage
    
    * set retention period for artifacts
    
    * move to yammbs 0.1.0 (identical to beta1)
    
    * add subset of industry dataset for testing
    
    * add try.yaml
    
    * Add benchmark results
    
    * remove try.yaml after successful test
    
    * Add benchmark results
    
    * typo
    
    * update readme
    
    * link typo
    
    ---------
    
    Co-authored-by: root <root@ip-172-31-77-26.ec2.internal>
    Co-authored-by: root <root@ip-172-31-64-158.ec2.internal>
    Co-authored-by: root <root@ip-172-31-72-137.ec2.internal>
    Co-authored-by: root <root@ip-172-31-77-238.ec2.internal>

--- datasets/OpenFF-Industry-Benchmark-Season-1-v1.1/README.md
+++ datasets/OpenFF-Industry-Benchmark-Season-1-v1.1/README.md
@@ -5,3 +5,12 @@ Namely, the dataset name and `chunksize` are now passed via the command line
 rather than being part of a config file (`industry.yaml` in this case). For that
 reason, a copy of the version of `download_and_filter_dataset.py` (previously
 called `new_dataset.py`) is also included here.
+
+Additionally, the cache file `first100.json` contains the first 100 entries in
+from `cache.json` for testing purposes. This file was produced by manipulating
+`cache.json` with `jq`:
+
+``` shell
+jq '{qm_molecules: .qm_molecules[:100], tag: .tag, version: .version}' cache.json > first100.json
+```
+
--- get_files.py
+++ get_files.py
@@ -1,11 +1,24 @@
 # read the config file supplied as the first argument and write the associated
-# files to stdout
+# files to stdout, if they exist
 
+import logging
 import sys
+from pathlib import Path
 
 from config import Config
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("get_files.py")
+
+
+def maybe_print(path):
+    if Path(path).exists():
+        print(path)
+    else:
+        logger.warning(f"skipping non-existent file: {path}")
+
+
 conf = Config.from_file(sys.argv[1])
-print(conf.forcefield)
+maybe_print(conf.forcefield)
 for ds in conf.datasets:
-    print(ds)
+    maybe_print(ds)
