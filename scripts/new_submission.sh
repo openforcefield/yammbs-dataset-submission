9c91b9ee8acfaafe73940aa35d5e3e1f00fce43a
Brent Westbrook <brentrwestbrook@gmail.com>
2024-11-19 13:50:08 -0500

    fix default dataset path

--- scripts/new_submission.sh
+++ scripts/new_submission.sh
@@ -8,7 +8,7 @@ mkdir -p ${sub_dir}
 cat > ${sub_dir}/input.yaml <<INP
 forcefield: openff_unconstrained-2.1.0.offxml
 datasets:
-    - datasets/cache/industry.json
+    - datasets/OpenFF-Industry-Benchmark-Season-1-v1.1/cache.json
 INP
 
 sed -i "/ENDOFTABLE/i | [$sub_name]($sub_dir) | TODO | [TODO](https://doi.org/TODO) |" README.md
